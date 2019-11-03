c74602b7dacc49937a874668732cab32f5b8ca18
Abdi <oxabdi75@gmail.com>
2019-11-03 20:24:10 +0300

    added javascript effect to the contact form

--- js/script.js
+++ js/script.js
@@ -42,4 +42,22 @@ $(document).ready(function() {
       .find(".road")
       .toggle(400);
   });
+  $("#contactUs").submit(function(event) {
+    var myName = $("#name").val();
+    var myEmail = $("#email").val();
+    var comment = $("#textarea").val();
+
+    if (myName === "" || myEmail === "" || comment === "") {
+      alert("Please fill in the required data");
+    } else {
+      alert(
+        myName +
+          " ,we have received your message.Thank you for reaching out to us."
+      );
+    }
+    event.preventDefault();
+    $("#name").val("");
+    $("#email").val("");
+    $("#textarea").val("");
+  });
 });
